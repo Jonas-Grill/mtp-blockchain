e250b161712ed6a66f571fa438427ae605617a17
elianderlohr <elias.anderlohr@t-online.de>
2022-10-18 16:38:15 +0200

    fix(cicd): test

---
+++ web3/test/ganache/setup_ganache.js
@@ -0,0 +1,7 @@
+const accounts = await web3.eth.getAccounts();
+const newAccount = await web3.eth.personal.newAccount('test');
+console.log("newAccount", newAccount);
+await web3.eth.personal.unlockAccount(newAccount, 'test', 10000);
+await web3.eth.getBalance(accounts[0], (err, bal) => { console.log("Ganache balance", bal); });
+await web3.eth.sendTransaction({ to: newAccount, from: accounts[0], value: web3.utils.toWei("5", "ether") });
+await web3.eth.getBalance(newAccount, (err, bal) => { console.log("New Account balance", bal); });
