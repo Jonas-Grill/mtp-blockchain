0a7760eb996466afa3c0a94bba17f1f8dad49f50
elianderlohr <elias.anderlohr@t-online.de>
2022-10-18 16:41:18 +0200

    fix(cicd): test

--- web3/test/ganache/setup_ganache.js
+++ web3/test/ganache/setup_ganache.js
@@ -1,7 +1,15 @@
-const accounts = await web3.eth.getAccounts();
-const newAccount = await web3.eth.personal.newAccount('test');
-console.log("newAccount", newAccount);
-await web3.eth.personal.unlockAccount(newAccount, 'test', 10000);
-await web3.eth.getBalance(accounts[0], (err, bal) => { console.log("Ganache balance", bal); });
-await web3.eth.sendTransaction({ to: newAccount, from: accounts[0], value: web3.utils.toWei("5", "ether") });
-await web3.eth.getBalance(newAccount, (err, bal) => { console.log("New Account balance", bal); });
+(async () => {
+    var Web3 = require("web3");
+
+    var web3 = new Web3('http://localhost:8545'); // your geth
+
+    const accounts = await web3.eth.getAccounts();
+    const newAccount = await web3.eth.personal.newAccount('test');
+
+    console.log("newAccount", newAccount);
+
+    await web3.eth.personal.unlockAccount(newAccount, 'test', 10000);
+    await web3.eth.getBalance(accounts[0], (err, bal) => { console.log("Ganache balance", bal); });
+    await web3.eth.sendTransaction({ to: newAccount, from: accounts[0], value: web3.utils.toWei("5", "ether") });
+    await web3.eth.getBalance(newAccount, (err, bal) => { console.log("New Account balance", bal); });
+})
