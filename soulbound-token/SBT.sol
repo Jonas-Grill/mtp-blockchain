e3ea28c0169e04d2042c8e4cba712b810d1c4af7
GinaWeber <57496524+GinaWeber@users.noreply.github.com>
2022-10-24 16:08:52 +0200

    Create SBT.sol

---
+++ soulbound-token/SBT.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+error SoulBoundRestriction();
+
+abstract contract ERC20SB {
+
+    //EVENTS
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    //METADATA STORAGE
+    string public name;
+
+    string public symbol;
+
+    uint8 public immutable decimals;
+
+    //ERC20 STORAGE
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    //CONSTRUCTOR
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
+
+    //ERC20SB LOGIC
+    function approve() public virtual returns (bool) {
+        revert SoulBoundRestriction();
+    }
+
+    function transfer() public virtual returns (bool) {
+        revert SoulBoundRestriction();
+    }
+
+    function transferFrom() public virtual returns (bool) {
+        revert SoulBoundRestriction();
+    }
+
+    //INTERNAL MINT / BURN LOGIC
+    function _mint(address to, uint256 amount) internal virtual {
+        totalSupply += amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount;
+        }
+
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal virtual {
+        balanceOf[from] -= amount;
+
+        // Cannot underflow because a user's balance
+        // will never be larger than the total supply.
+        unchecked {
+            totalSupply -= amount;
+        }
+
+        emit Transfer(from, address(0), amount);
+    }
+}
