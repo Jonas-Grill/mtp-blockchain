62ed001045783ba1fe859855dedfe0a88bf1e68b
elianderlohr <elias.anderlohr@t-online.de>
2022-11-17 19:22:56 +0100

    fix: cicd

--- web3/src/web3/utils.js
+++ web3/src/web3/utils.js
@@ -39,21 +39,6 @@ class UniMaUtils {
 
         let json = fs.readFileSync(filePath, 'utf8');
 
-        console.log("Path: " + directoryPath)
-
-        //passsing directoryPath and callback function
-        fs.readdir(directoryPath, function (err, files) {
-            //handling error
-            if (err) {
-                return console.log('Unable to scan directory: ' + err);
-            }
-            //listing all files using forEach
-            files.forEach(function (file) {
-                // Do whatever you want to do with the file
-                console.log(file);
-            });
-        });
-
         return JSON.parse(json);
     }
 
