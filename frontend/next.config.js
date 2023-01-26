a7c240f5104d104bd06d104952bb1c64b52660b7
Jonas-Grill <jonasb.grill@gmail.com>
2023-01-26 12:56:58 +0100

    fix: change frontend access to ganache chain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    RPC_URL: "http://192.168.178.68:8506",
+    RPC_URL: "http://127.0.0.1:8545",
+    // RPC_URL: "http://192.168.178.68:8506",
     NETWORK_ID: "1337"
   }
 }
