ec8b10ed79e94f10c4e2769d5dfcda0f394bc061
Jonas-Grill <jonasb.grill@gmail.com>
2023-01-29 14:50:26 +0100

    ass submitAssignment popover

--- frontend/pages/submitAssignment/index.tsx
+++ frontend/pages/submitAssignment/index.tsx
@@ -1,4 +1,4 @@
-import {ArrowUpTrayIcon, DocumentMagnifyingGlassIcon} from '@heroicons/react/20/solid'
+import {ArrowUpTrayIcon, DocumentMagnifyingGlassIcon, ExclamationCircleIcon} from '@heroicons/react/24/outline'
 import React, {useEffect, useState} from "react";
 import {
     getTestIndexes,
@@ -12,10 +12,16 @@ import {Assignment, loadAssignments} from "../assignments";
 import Head from "next/head";
 import {initBlockchain} from "../faucet";
 import {isAdmin} from "../../web3/src/entrypoints/config/admin";
+import { Fragment, useRef} from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
 export default function SubmitAssignment({userAddress}: { userAddress: string }) {
     const [web3, setWeb3] = useState<any>(undefined);
     const [contract, setContract] = useState<string>("");
+
+    const [open, setOpen] = useState(false)
+    const cancelButtonRef = useRef(null)
 
     const [semesters, setSemesters] = useState<Semester[]>([]);
     const [selectedSemester, setSelectedSemester] = useState<string>("");
@@ -30,6 +36,14 @@ export default function SubmitAssignment({userAddress}: { userAddress: string })
 
     const handleTestAssignment = async (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
+
+        if (contract === "") {
+            alert("Please enter your contract address")
+            return
+        } else if (!web3.utils.isAddress(contract)) {
+            alert("Please enter a valid contract address")
+            return
+        }
 
         if (web3) {
             const assignment = assignments.find(assignment => assignment.id === selectedAssignment);
@@ -172,6 +186,73 @@ export default function SubmitAssignment({userAddress}: { userAddress: string })
                             Hand in your assignment contract here
                         </h2>
                     </div>
+                    <Transition.Root show={open} as={Fragment}>
+                        <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                            </Transition.Child>
+
+                            <div className="fixed inset-0 z-10 overflow-y-auto">
+                                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                    <Transition.Child
+                                        as={Fragment}
+                                        enter="ease-out duration-300"
+                                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                        enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                        leave="ease-in duration-200"
+                                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                    >
+                                        <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                                <div className="sm:flex sm:items-start">
+                                                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-300 sm:mx-0 sm:h-10 sm:w-10">
+                                                        <ExclamationCircleIcon className="h-6 w-6 text-uni" aria-hidden="true" />
+                                                    </div>
+                                                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                                        <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-uni">
+                                                            Submit assignment
+                                                        </Dialog.Title>
+                                                        <div className="mt-2">
+                                                            <p className="text-sm text-gray-600">
+                                                                Are you sure you want to submit your assignment?
+                                                                You can only submit once. This action cannot be undone.
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                                <button
+                                                    type="button"
+                                                    className="inline-flex w-full justify-center rounded-md bg-gray-300 px-4 py-2 text-base font-medium text-uni hover:bg-uni hover:text-white focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"
+                                                    onClick={handleSubmitAssignment}
+                                                >
+                                                    Submit
+                                                </button>
+                                                <button
+                                                    type="button"
+                                                    className="mt-3 inline-flex w-full justify-center rounded-md bg-gray-300 px-4 py-2 text-base font-medium text-uni hover:bg-uni hover:text-white focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                                    onClick={() => setOpen(false)}
+                                                    ref={cancelButtonRef}
+                                                >
+                                                    Cancel
+                                                </button>
+                                            </div>
+                                        </Dialog.Panel>
+                                    </Transition.Child>
+                                </div>
+                            </div>
+                        </Dialog>
+                    </Transition.Root>
                     {
                         isUserAdmin ? (
                             <>
@@ -237,7 +318,16 @@ export default function SubmitAssignment({userAddress}: { userAddress: string })
 
                     <div className="py-6">
                         <button
-                            onClick={handleSubmitAssignment}
+                            onClick={() => {
+                                if (contract === "") {
+                                    alert("Please enter your contract address")
+                                    return
+                                } else if (!web3.utils.isAddress(contract)) {
+                                    alert("Please enter a valid contract address")
+                                    return
+                                }
+                                setOpen(true)
+                            }}
                             className="group relative flex w-full justify-center rounded-md shadow shadow-uni bg-gray-400 py-2 px-4 text-sm font-medium text-uni hover:bg-uni hover:text-white"
                         >
                             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
